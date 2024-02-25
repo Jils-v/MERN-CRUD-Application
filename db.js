4aaa5d7d437df0b681cf6cb00426f51f08e2cc19
Jils-v <jilsvaghasiya333@gmail.com>
2024-02-25 13:59:33 +0530

    DB info added

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 module.exports = () => {
 	const connection = mongoose
-		.connect(process.env.MONGODB_URI)
+		.connect(process.env.MONGODB_URI,
+			{
+				useNewUrlParser: true,
+				ssl: true,
+				sslValidate: true,
+				sslCA: `global-bundle.pem`
+			})
 		.then((result) => console.log("Connected to database"))
-		.catch((err) => console.log("could not connect to database"));
+		.catch((err) => console.log("could not connect to database", err));
 };
